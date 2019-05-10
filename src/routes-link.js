41fca0272ff4b93bb5985326f1c6e83e54f20f5f
colin <colinscott1@gmail.com>
2019-05-09 22:43:53 -0400

    Re-register link on href change for correct status

--- src/routes-link.js
+++ src/routes-link.js
@@ -1,18 +1,39 @@
 ///@ts-check
 import {RouterElement} from './routes-router.js';
 
+/** Helper to dispatch events that will signal the registering of links. */
+const dispatchRegisterLink = function(link) {
+  window.dispatchEvent(
+    new CustomEvent(
+        'routerLinksAdded', {
+            detail: {
+                links: [link] }}));
+}
+
 class RouterLinkElement extends HTMLAnchorElement {
+  /** @inheritdoc */
   connectedCallback() {
-
     RouterElement.initialize();
-
-    window.dispatchEvent(
-      new CustomEvent(
-          'routerLinksAdded', {
-              detail: {
-                  links: [this] }}));
+    dispatchRegisterLink(this);
+    
   }
 
+  /** @inheritdoc */
+  static get observedAttributes() { return ['href']; }
+
+  /**
+   * @inheritdoc
+   * Listens for href attribute changing. If it does then it re-regesters the link.
+   */
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'href') {
+      if (oldValue && newValue) {
+        dispatchRegisterLink(this);
+      }
+    }
+  }
+
+  /** @inheritdoc */
   constructor() {
     super();
   }
