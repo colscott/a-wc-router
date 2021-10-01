4c2b23d841dcede192ac5c545c0c341434eb6a32
colin <colinscott1@gmail.com>
2021-10-01 07:58:10 -0400

    feat(router): export all exportables from entry file

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
-import './named-routing.js';
-import './routes-router.js';
-import './routes-route.js';
-import './routes-outlet.js';
 import './routes-link.js';
+
+export * from './named-routing.js';
+export * from './routes-router.js';
+export * from './routes-route.js';
+export * from './routes-outlet.js';
